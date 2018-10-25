3992e0b28f67707b99b29e8a7f520b000cafb1fb
Jack Franklin <jack@jackfranklin.net>
2018-10-25 11:30:52 +0100

    I can render a react thing !

--- example/example.js
+++ example/example.js
@@ -1,2 +1,23 @@
-import CustomReactElements from '../src/index';
-console.log('example!');
+// import CustomReactElements from '../src/index';
+
+import React from 'react';
+import App from './component';
+import ReactDOM from 'react-dom';
+
+class WordCount extends HTMLElement {
+  constructor() {
+    super();
+    console.log('constrcutor called');
+  }
+
+  connectedCallback() {
+    let shadowRoot = this.attachShadow({ mode: 'open' });
+    const x = document.createElement('div');
+    ReactDOM.render(<App />, x);
+    shadowRoot.appendChild(x);
+  }
+}
+
+console.log('got here', WordCount);
+
+customElements.define('react-example-component', WordCount);
